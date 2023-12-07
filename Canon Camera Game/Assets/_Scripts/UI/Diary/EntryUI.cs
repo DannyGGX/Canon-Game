25e885245401f23f1d69c9fe87d5dc46233b8698
DannyGGX <danielglanz111@gmail.com>
2023-12-07 19:05:22 +0200

    Added Lens UI
    UI to show the currently equipped lens.
    Removed unnecessary scripts and folders.

--- Canon Camera Game/Assets/_Scripts/UI/Diary/EntryUI.cs
+++ Canon Camera Game/Assets/_Scripts/UI/Diary/EntryUI.cs
@@ -13,7 +13,7 @@ public class EntryUI : MonoBehaviour
     public void SetEntryInfo(string description, LensTypes lensType)
     {
         descriptionText.text = description;
-        lensText.text = EnumToString(lensType);
+        lensText.text = lensType.EnumToString();
     }
 
     public void SetEntryInfo(string description)
@@ -25,40 +25,5 @@ public class EntryUI : MonoBehaviour
     public void SetEntryComplete()
     {
         descriptionText.fontStyle = FontStyles.Strikethrough;
-    }
-
-    private static string EnumToString(Enum @enum)
-    {
-        return InsertSpaces(@enum.ToString());
-    }
-    private static string InsertSpaces(string input)
-    {
-        if (string.IsNullOrEmpty(input))
-        {
-            return input;
-        }
-
-        StringBuilder result = new StringBuilder();
-        result.Append(input[0]); // Add the first character as is
-
-        for (int i = 1; i < input.Length; i++)
-        {
-            char currentChar = input[i];
-
-            // Add a space before capital letters (except for the first letter)
-            if (char.IsUpper(currentChar) && i > 1)
-            {
-                result.Append(' ');
-            }
-            // Add a space before any amount of digits
-            else if (char.IsDigit(currentChar) && !char.IsDigit(input[i - 1]))
-            {
-                result.Append(' ');
-            }
-
-            result.Append(currentChar);
-        }
-
-        return result.ToString();
     }
 }
